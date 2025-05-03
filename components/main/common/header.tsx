584b4859e5489206c54cd93c2ddd34c5b74f958e
Kyavawa Rashidah <kyavawa.rashidah@student.mak.ac.ug>
2025-05-03 22:57:35 +0300

    added authcontext

--- components/main/common/header.tsx
+++ components/main/common/header.tsx
@@ -10,7 +10,7 @@ export function Header({ user }: HeaderProps) {
   const isAuthenticated = !!user;
 
   if (isAuthenticated) {
-    return <Navbar user={use/>;
+    return <Navbar />;
   }
 
   return <PublicHeader />;
